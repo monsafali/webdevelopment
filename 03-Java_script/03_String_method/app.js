735fb51bf17ee7509af9f13056354fb977924eb9
monsafali <monsafali272@gmail.com>
2024-04-20 09:01:16 +0500

    string and their method

---
+++ 03-Java_script/03_String_method/app.js
@@ -0,0 +1,73 @@
+// let msg = "     hello       "
+// let clear = msg.trim();
+// const name = 'John';
+// const message = `Hello, ${name}!`;
+// console.log(message)
+
+// console.log(msg)
+// console.log(clear)
+
+// let passwod = prompt('set yoru password');
+// console.log(passwod.trim())
+// let Name = 'Monsaf Ali'
+// console.log(Name.toUpperCase());
+// console.log(Name.toLowerCase());
+
+// we also return a object 
+// const stringObject = new String('This is a string');
+
+
+
+// finding specific index number
+// let x = '       That was AmaZing session.  ';
+// console.log(x.indexOf('random'));
+// // if value not exist in  a string it return -1 index number which means this specific word not exit in the whole strin
+// console.log(x.indexOf('love'));
+
+
+// // method changing means we apply many method in one line
+// let clean = x.trim().toLowerCase();
+// // stice method
+// console.log(clean.slice(0,8));
+// console.log(clean.slice(-3));
+// // find lenght
+// console.log(clean.length);
+
+// // Replace String
+// let str = "Hello World!";
+// // repeat
+// console.log(str.repeat(3)); 
+// let New = str.replace('Hello','Hi')
+
+// console.log(New);
+
+// let text = "Mr Blue has a blue house and a blue car";
+// let result = text.replace(/blue/gi, "red");
+// console.log(result);
+// //Result: Mr red has a red house and a red car
+
+
+// // string Concatination
+// let F_name = 'monsaf';
+// let S_name = "Ali";
+// let full_name = F_name.concat(" ",S_name);
+// console.log(full_name)
+
+// Split    The split() method divides a string into a list of substrings and returns them as an array
+
+
+// let txt = 'the quick brown';
+// let array = txt.split(" ");
+// console.log(array);
+
+// const text2 = "JavaScript ;Python ;C;C++";
+// let splited = ";";
+// let newText1 = text2.split(splited,3);  // we also give a number how many length data we need
+// console.log(newText1)
+
+// Substring
+
+// let details = "i love coding my base field is cybersecurity";
+// let sub = details.substring(30);
+// console.log(sub)
+
