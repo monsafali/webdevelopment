a769d04c849638b65462ee0083de9bb9ef854aeb
monsafali <monsafali272@gmail.com>
2024-05-02 08:40:36 +0500

    object Literal

---
+++ 03-Java_script/08_Object_Literals/app.js
@@ -0,0 +1,108 @@
+// let students = {
+//    Name : "monsaf",
+//    age : 25,
+//    marks: 64,
+//    city : "Fort Abbas"
+// }
+
+// console.log(students.Name)
+
+
+
+// let thread_twitter = {
+//    User_name : "monsaf Ali",
+//    content: "This is my First Post",
+//    contanct: +923116940272,
+//    likes:  +100,
+//    repost: 92,
+//    tags: ["apnacollege", "delta"]
+// }
+
+// console.log(thread_twitter['User_name']);
+// console.log(thread_twitter.User_name)
+
+
+
+// const obj = {
+//    1: "a",
+//    2: "b",
+//    true: "c",
+//    null: "d",
+//    undefined: "e"
+// };
+
+
+
+let students = {
+   Name : "monsaf",
+   age : 25,
+   marks: 64,
+   city : "Fort Abbas"
+}
+
+
+// if we want to change our existing value 
+students.city = "lahore";
+students.gender = "male";
+students.marks = [99,78,68]
+
+// if i want to delte the key
+
+// delete students.city
+
+
+// const classInfo = {
+//    amna: {
+//       grade: "A",
+//       city: "laore"
+
+//    },
+//    laiba: {
+//       grade :"A++",
+//       city: "Quetta"
+//    },
+//    Ayesa: {
+//       grade: "B++",
+//       City: "Karahi"
+//    }
+// }
+
+// console.log(classInfo)
+// console.log(classInfo.laiba.city)
+
+
+
+// Array Of Objects
+// let classroom2 = [
+//    {
+//    Name:"monsaf",
+//    grade : "A",
+//     City: "Fort Abbas"
+//    },
+//    {
+//       Name : "Laiba",
+//       Grade : "A++",
+//       City : "Quette"
+//    },
+//    {
+//       Name : "Faisal",
+//       Grade: "A++++",
+//       City: "Canada"
+//    }
+// ]
+
+// console.log(classroom2[2]['Name']);
+
+
+
+
+
+
+
+
+// Random Integers
+
+// it return the number between one and ten
+
+let Number =  Math.floor(Math.random()*10)+1;
+console.log(Number)
