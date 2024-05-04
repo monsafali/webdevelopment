398f57903fc1ef221d050c36b34ec0ba1bd601a0
monsafali <monsafali272@gmail.com>
2024-05-04 20:21:46 +0500

    scope of funciton

--- 03-Java_script/09_Function/app.js
+++ 03-Java_script/09_Function/app.js
@@ -102,3 +102,127 @@ function outerfunc(){
 }
 
 
+
+let great = "hellow";
+function ChangeGreet(){
+    let great = "Asllamaoalikum";
+    // console.log(great);
+    function innerGreat(){
+        console.log("Monsaf",great);
+    }
+    innerGreat()
+}
+
+// console.log(great);
+// console.log(ChangeGreet());
+// console.log(innerGreat());
+
+
+// Function Expression
+let sum2 = function(a,b){
+    console.log(a+b)
+}
+console.log(sum2(11,11));
+
+
+
+// higher order funtion
+
+
+function multiplegreet(fun, n){
+    for(let i=1; i<n; i++){
+        fun()
+    }
+}
+function big(){
+    console.log('Welcome to a big game')
+}
+
+// console.log(multiplegreet(big, 10))
+
+
+// Higher order Function
+// Return a Function
+
+let odd = function(n){
+    console.log(!(n%2 == 0));
+}
+
+let even = function(n){
+    console.log(n%2 == 0);
+}
+
+
+
+function oddeveTest(request){
+    if(request === "odd"){
+        let odd = function(n){
+            console.log(!(n%2 == 0));
+        }
+        return odd;
+    }
+    else if(request === "even"){
+        let even = function(n){
+            console.log(n%2 == 0);
+        }
+        return even;
+    }
+     else{
+        console.log('wrong request')
+    }
+}
+
+let request = "odd";
+
+
+
+
+// One MOre example
+
+
+// console.log(double(4));
+// console.log(add_2(4));
+
+let map = function (func, list){
+    let output = [];
+    for(idx in list){
+        output.push(func(list[idx]));
+    }
+    return output;
+}
+
+let add_2 = function (x){
+    return x +2;
+ }
+
+ let double = function (x){
+    return x*2;
+ }
+
+map(add_2,[5, 6, 7]);
+map(double, [5, 6, 7]);
+
+
+
+
+// Methods
+
+const calculator = {
+    num: 88,
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function (a,b){
+        return a-b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    devidided: function(a,b){
+        return a/b
+    }
+}
+
+ calculator(add(2,3));
+ 
+
