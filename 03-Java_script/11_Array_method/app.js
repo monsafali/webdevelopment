a9ba0e7f8be9176c311820b8a3674d3027babe95
monsafali <monsafali272@gmail.com>
2024-05-08 17:14:08 +0500

    reduced method in array

--- 03-Java_script/11_Array_method/app.js
+++ 03-Java_script/11_Array_method/app.js
@@ -1,10 +1,10 @@
 // For each method in array
 
-let array = [1,2,3,4,5,6];
+// let array = [1,2,3,4,5,6];
 
-let print = function(arr){
-    console.log(arr);
-}
+// let print = function(arr){
+//     console.log(arr);
+// }
 
 // array.forEach(print);
 
@@ -13,73 +13,73 @@ let print = function(arr){
 // })
 
 // we used forEach in arrow funciton
-array.forEach((arr)=>{
-    console.log(arr)
-})
+// array.forEach((arr)=>{
+//     console.log(arr)
+// })
 
 // we used it at ojbect in array
 
-let arr = [
-    {
-        Name: "monsaf",
-        marks: 23,
-    },
-    {
-        Name: "laiba",
-        marks: 78,
-    },
-    {
-        Name: "ayesha",
-        marks: 50,
-    }
-]
+// let arr = [
+//     {
+//         Name: "monsaf",
+//         marks: 23,
+//     },
+//     {
+//         Name: "laiba",
+//         marks: 78,
+//     },
+//     {
+//         Name: "ayesha",
+//         marks: 50,
+//     }
+// ]
 
-arr.forEach((student)=>{
-    console.log(student.Name);
-})
+// arr.forEach((student)=>{
+//     console.log(student.Name);
+// })
 
 
 
 // Map 
 
-let num = [2,4,5,6,8,10]
-let double = num.map((element)=>{
-    return element*2
-})
-console.log(double)
+// let num = [2,4,5,6,8,10]
+// let double = num.map((element)=>{
+//     return element*2
+// })
+// console.log(double)
 
 
 
 
 
 
-let studentgpa = [
-    {
-        Name: "monsaf",
-        marks: 23,
-    },
-    {
-        Name: "laiba",
-        marks: 78,
-    },
-    {
-        Name: "ayesha",
-        marks: 50,
-    }
-]
+// let studentgpa = [
+//     {
+//         Name: "monsaf",
+//         marks: 23,
+//     },
+//     {
+//         Name: "laiba",
+//         marks: 78,
+//     },
+//     {
+//         Name: "ayesha",
+//         marks: 50,
+//     }
+// ]
 
-let gpa = studentgpa.map((el)=>{
-    return el.marks /10;
-})
-console.log(gpa)
+// let gpa = studentgpa.map((el)=>{
+//     return el.marks /10;
+// })
+// console.log(gpa)
 
 
 // filter
 
-let nums = [1,5,4,2,7,8,12,3,4,17,19,20];
-let ans = nums.filter((el)=>{
-    return el %2 == 1;
-})
+// let nums = [1,5,4,2,7,8,12,3,4,17,19,20];
+// let ans = nums.filter((el)=>{
+//     return el %2 == 1;
+// })
 
 
 // every Method
@@ -89,5 +89,36 @@ let ans = nums.filter((el)=>{
 // Some
 
 // [2,4,6,8,10].some((el) => el%2==0);
+
+
+// let num1 = [1,2,3,4];
+// let final_value = num1.reduce((res, el)=> res + el);
+// console.log(final_value);
+
+
+
+// find the maximum number from array
+let arr = [2,3,3,23,23,43,3,34];
+let max = -1;
+for(let i=0; i<arr.length; i++){
+    if(max < arr[i]){
+        max = arr[i];
+    }
+}
+// console.log(max);
+// using reduced method
+let maximum = arr.reduce((min, max)=>{
+    if(min>max){
+        return min;
+    }else {
+        return max;
+    }          
+})
+
+
+
+[20,40,30,20,10].every((el) => el%10==0);
+
+
 
 
