98b6ceb8c35b8b674f080a21605663520b1e4442
monsafali <monsafali272@gmail.com>
2024-05-10 17:18:08 +0500

    DOM in javascript

---
+++ 03-Java_script/12_DOM/app.js
@@ -0,0 +1,23 @@
+let imag = document.getElementById("mainImg");
+
+let small_images = document.getElementsByClassName("oldimg");
+
+// for (let i=0; i<small_images.length; i++){
+//    small_images[i].src = "Assets/Spider-Man.png"
+//    console.log(`value of image no. ${i} is changed`)
+// }
+
+
+
+// Query Selector
+
+let inside = document.querySelector('div a')
+console.dir(inside)
+
+let h1 = document.querySelector('h1');
+console.dir(h1);
+let mainImg = document.querySelector('#mainImg');
+console.dir(mainImg);
+let oldimg = document.querySelectorAll('.oldimg');
+console.dir(oldimg);
+
