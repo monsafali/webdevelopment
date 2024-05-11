ce18b9ec70dc63bcaf8c439846c7d8f4de2c22fa
monsafali <monsafali272@gmail.com>
2024-05-11 08:39:36 +0500

    manipulating attriube style etc

--- 03-Java_script/12_DOM/app.js
+++ 03-Java_script/12_DOM/app.js
@@ -11,13 +11,41 @@ let small_images = document.getElementsByClassName("oldimg");
 
 // Query Selector
 
-let inside = document.querySelector('div a')
-console.dir(inside)
+// let inside = document.querySelector('div a')
+// console.dir(inside)
 
+// let h1 = document.querySelector('h1');
+// console.dir(h1);
+// let mainImg = document.querySelector('#mainImg');
+// console.dir(mainImg);
+// let oldimg = document.querySelectorAll('.oldimg');
+// console.dir(oldimg);
+
+
+
+
+// Manipulating Attributes:
+
+
+// let img = document.querySelector('img');
+// let id_name = img.getAttribute('id');
+// console.dir(id_name);
+// let change_id_name = img.setAttribute('id','topimg');
+// console.dir(change_id_name);
+
+
+
+// manipulating style:
 let h1 = document.querySelector('h1');
-console.dir(h1);
-let mainImg = document.querySelector('#mainImg');
-console.dir(mainImg);
-let oldimg = document.querySelectorAll('.oldimg');
-console.dir(oldimg);
+h1.style.backgroundColor = 'black'
+h1.style.color = 'white';
 
+
+let links = document.querySelectorAll(".pubInfo a");
+for(let i=0; i<links.length; i++){
+    links[i].style.color = 'white'
+}
+
+for(link of links){
+    link.style.backgroundColor = 'black'
+}
