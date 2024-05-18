20fb428cb49ff213217a58a6952190bcbf19f2c6
monsafali <monsafali272@gmail.com>
2024-05-18 08:10:44 +0500

    todo app

---
+++ 03-Java_script/14_Todo_app/app.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector('button')
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
+
+btn.addEventListener('click', function(){
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add('delete');
+
+    item.appendChild(delBtn)
+    ul.appendChild(item);
+    inp.value = "";
+})
+
+
+let delBtns = document.querySelectorAll('.delete');
+for(delBtn of delBtns){
+    delBtn.addEventListener('click', function(){
+        // console.log('element deleted successfuly')
+
+        let parent = this.parentElement;
+        console.log(parent)
+        parent.remove();
+    })
+}
